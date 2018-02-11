5db8589bfbd2f52f3263fe628a203cd80a61dad0
Your Name <you@example.com>
2018-02-11 19:51:51 +0300

    mappers

---
+++ src/main/java/com/mycompany/mavenproject3/dao/type/geners.java
@@ -0,0 +1,23 @@
+package com.mycompany.mavenproject3.dao.type;
+public class geners {
+   private int id_geners;
+   private String name;
+
+    public int getId_geners() {
+        return id_geners;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId_geners(int id_geners) {
+        this.id_geners = id_geners;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+   
+    
+}
---
+++ src/main/java/com/mycompany/mavenproject3/mappers/descriptionMappers.java
@@ -0,0 +1,4 @@
+package com.mycompany.mavenproject3.mappers;
+public interface descriptionMappers {
+    
+}
